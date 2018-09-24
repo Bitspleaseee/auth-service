db8a7b58cd3cb1c0762abcb9356b55da6e31ed60
Uran Hajzeraj <uhajzeraj@hotmail.com>
2018-09-24 17:00:11 +0200

    struct/enum added

---
+++ src/test.rs
@@ -0,0 +1,9 @@
+// use std::io::{stdin, stdout, Write};
+
+
+// let mut s = String::new();
+// print!("Please enter some text: ");
+// let _ =stdout().flush();
+// stdin().read_line(&mut s).expect("Did not enter a string");
+
+// println!("You typed: {}", s);
