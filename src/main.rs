4e1541a68c52053dcf1f6fd50a960acc740aaf06
Uran Hajzeraj <uhajzeraj@hotmail.com>
2018-09-25 02:06:18 +0200

    fixed parse_json_data function return issue

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,46 @@
-// extern crate serde;
 extern crate serde_json;
 
-#[macro_use]
-extern crate serde_derive;
+use serde_json::{Value,Error};
 
-#[derive(Serialize, Deserialize)]
-enum UserRole {
-    Guest,
-    User,
-    Moderator,
-    Administrator,
-}
-
-#[derive(Serialize, Deserialize)]
-struct User {
-    id: u32,
-    email: String,
-    username: String,
-    password: String,
-    salt: String,
-    role: UserRole,
-    banned: bool,
-    verified: bool,
-    email_token: String,
-}
-
-fn get_register_info(json_data: String) -> (String, String, String) {
-
-    let u: User = serde_json::from_str(json_data);
-
-    // println!("{} {} {}", u.email, u.username, u.password);
-    (u.email, u.username, u.password)
-}
-
-// fn untyped_example() -> Result<(), Error> {
-//     // Some JSON input data as a &str. Maybe this comes from the user.
-//     let data = r#"{
-//                     "name": "John Doe",
-//                     "age": 43,
-//                     "phones": [
-//                       "+44 1234567",
-//                       "+44 2345678"
-//                     ]
-//                   }"#;
-
-//     // Parse the string of data into serde_json::Value.
-//     let v: Value = serde_json::from_str(data)?;
-
-//     // Access parts of the data by indexing with square brackets.
-//     println!("Please call {} at the number {}", v["name"], v["phones"][1]);
-
-//     Ok(())
+// enum UserRole {
+//     Guest,
+//     User,
+//     Moderator,
+//     Administrator,
 // }
 
-fn main() {
-    //The JSON data received from another module
-    let json_data = r#"{
-                        "email": "test@somedomain.com",
-                        "username": "someUsername",
-                        "password": "someuserpass"
-                      }"#;
+// struct User {
+//     id: u32,
+//     email: String,
+//     username: String,
+//     password: String,
+//     salt: String,
+//     role: UserRole,
+//     banned: bool,
+//     verified: bool,
+//     email_token: String,
+// }
 
-    get_register_info(json_data);
+fn parse_json_data(s: &str) -> Result<Value, Error> {
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(&s)?;
+    Ok(v)
+}
+
+fn main() {
+
+    // Static JSON data, for now...
+    let x = r#"{
+                    "email": "test@somedomain.com",
+                    "username": "someUsername",
+                    "password": "someuserpass"
+                  }"#;
+
+    // serde_json::from_str seems to return a Result type
+    // This is the only way I could get Result<T, E>
+    // to work while returning the parsed JSON data to main()
+    match parse_json_data(x) {
+        Ok(v) => println!("{}", v["email"]),
+        Err(v) => println!("Error {}", v),
+    }
 }
