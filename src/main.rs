e73907b3723c4fb0bd4b670d87c04aaa8c7425d5
Knut <knutgro@stud.ntnu.no>
2018-10-01 14:53:40 +0200

    1

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,9 @@
+extern crate diesel;
 extern crate serde_json;
 use serde_json::{Value, Error};
+use diesel::prelude::*;
 
-// enum UserRole {
-//     Guest,
-//     User,
-//     Moderator,
-//     Administrator,
-// }
-
-// struct User {
-//     id: u32,
-//     email: String,
-//     username: String,
-//     password: String,
-//     salt: String,
-//     role: UserRole,
-//     banned: bool,
-//     verified: bool,
-//     email_token: String,
-// }
-
-
-fn untyped_example() -> Result<(), Error> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"{
-                    "name": "John Doe",
-                    "age": 43,
-                    "phones": [
-                      "+44 1234567",
-                      "+44 2345678"
-                    ]
-                  }"#;
-
-    // Parse the string of data into serde_json::Value.
-    let v: Value = serde_json::from_str(data)?;
-
-    // Access parts of the data by indexing with square brackets.
-    println!("Please call {} at the number {}", v["name"], v["phones"][1]);
-
-    Ok(())
-}
+ 
 
 fn main() {
-    untyped_example();
 }
