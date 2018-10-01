7daf9a562c8ba255b688ce6ecabd33e2b6a36113
Uran Hajzeraj <uhajzeraj@hotmail.com>
2018-10-01 23:30:04 +0200

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+extern crate serde;
 extern crate serde_json;
 extern crate pbkdf2;
 extern crate regex;
+extern crate chrono;
+
+#[macro_use]
+extern crate serde_derive;
 
 use regex::Regex;
 use serde_json::{Value,Error};
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::File;
+
+#[derive(Serialize, Deserialize)]
+struct JsonData {
+    data_type: String,
+    payload: Payload,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Payload {
+    username: String,
+    // password: String
+}
 
 fn parse_json_data(s: &str) -> Result<Value, Error> {
     // Parse the string of data into serde_json::Value.
@@ -23,7 +44,7 @@ fn strip_characters(original : String, to_strip : &str) -> String {
 
 // I URAN, AM NOT PROUD OF THIS FUNCTION !
 // password - letters (lowercase AND uppercase) AND numbers, 8-50 characters
-fn check_password(input: &str) -> bool {
+fn check_pass_requirement(input: &str) -> bool {
     
     // check password length
     // check if password has numbers, then check for lowercase letters, then check for uppercase letters
@@ -46,6 +67,43 @@ fn check_password(input: &str) -> bool {
     return false;
 }
 
+
+// Authentication part - TO DO
+fn authenticate() {
+
+}
+
+// Create log for login/register attempt
+fn create_log(request : &str, username: &str) {
+
+    let file_name = "logFile";
+
+    // Check if file exists
+    // If it doesn't, create it
+    let file_exists : bool = Path::new(file_name).exists();
+    if !file_exists {
+        if let Err(e) = File::create(file_name) {
+            eprintln!("Couldn't create file: {}", e);
+            return;
+        }
+    }
+
+    // Document the time
+    let now = chrono::Local::now();
+    let time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+    // Write to file
+    let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open(file_name)
+                    .unwrap();
+
+    if let Err(e) = writeln!(file, "[{}]: {} from user {}", time, request, username) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+}
+
 fn main() {
 
     // Some variables
@@ -55,16 +113,16 @@ fn main() {
     let hashed_password : String;
 
     // This is easier to work with
-    let x = r#"{
-        "type": "AUTHENTICATE",
+    let json_data = r#"{
+        "type": "Authenticate",
         "username": "james",
-        "password": "testPass321"
+        "password": "TestPass321"
     }"#;
 
     // serde_json::from_str seems to return a Result type
     // This is the only way I could get Result<T, E>
     // to work while returning the parsed JSON data to main()
-    match parse_json_data(x) {
+    match parse_json_data(json_data) {
         Ok(_v) => {
             request = _v["type"].to_string();
             username = _v["username"].to_string();
@@ -81,26 +139,20 @@ fn main() {
     let request = strip_characters(request, r#"""#);
     let username = strip_characters(username, r#"""#);
     let password = strip_characters(password, r#"""#);
-    
-    // Regular Expression magic
-    // username - letters (lowercase AND/OR uppercase) OR numbers, 5-20 characters
-    let re = Regex::new(r"^[a-zA-Z0-9]{5,20}$").unwrap();
-    if re.is_match(&username) {
-        println!("Username matches the requirements");
-    } else {
-        println!("Username does not match the requirements");
-        return;
-    }
+
+    // Add log info to the log file
+    create_log(&request, &username);
 
     // password - letters (lowercase AND uppercase) AND numbers, 8-50 characters
     // RegEx in Rust doesn't support look ahead assertion, so the RegEx would be very complicated
-    if check_password(&password) {
-        println!("It is 8-50 characters");
+    if check_pass_requirement(&password) {
+        println!("Password matches requirements");
     } else {
+        println!("Password doesn't match requirements");
         return;
     }
 
-    // Hash the password
+    // // Hash the password
     match pbkdf2::pbkdf2_simple(&password, 1000) {
         Ok(_hash) => {
             hashed_password = _hash;
@@ -110,7 +162,7 @@ fn main() {
         }
     }
 
-    // Check the password hash against the password provided
+    // // Check the password hash against the password provided
     match pbkdf2::pbkdf2_check(&password, &hashed_password) {
         Ok(_x) => println!("Password matches password hash"),
         Err(_x) => println!("Password did not match password hash")
